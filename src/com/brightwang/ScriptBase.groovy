2d0d9011b8cd54822add0bbd11043c3816d174d2
brightwang <brightwang1984@gmail.com>
2018-08-06 22:55:13 +0800

    add test

--- src/com/brightwang/ScriptBase.groovy
+++ src/com/brightwang/ScriptBase.groovy
@@ -2,10 +2,11 @@ package com.brightwang
 
 abstract class ScriptBase extends Script {
     def excludeDir(dirs){
-
+        new File('./dir.txt').write('dir')
     }
 
     def excludeFile(files){
+        new File('./file.txt').write('file')
 
     }
 }
