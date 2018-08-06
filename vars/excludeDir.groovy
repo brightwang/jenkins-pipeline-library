8d2f3100ec5c9d0955c4f511cc2f5673e522285f
brightwang <brightwang1984@gmail.com>
2018-08-06 16:54:45 +0800

    add test

--- vars/excludeDir.groovy
+++ vars/excludeDir.groovy
@@ -1,5 +1,7 @@
 def call(String[] dirs){
-
+    generalBuildXml=new com.brightwang.GeneralBuildXml(env.xml)
+    generalBuildXml.excludeDir(dirs)
+    env.xml=generalBuildXml.getXmlString()
 }
 
 return this
--- vars/excludeFile.groovy
+++ vars/excludeFile.groovy
@@ -1,5 +1,7 @@
 def call(String[] files){
-
+    generalBuildXml=new com.brightwang.GeneralBuildXml(env.xml)
+    generalBuildXml.excludeFile(files)
+    env.xml=generalBuildXml.getXmlString()
 }
 
 return this
