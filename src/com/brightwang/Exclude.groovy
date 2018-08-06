2362b76615ae910df8301d005429799309322946
brightwang <brightwang1984@gmail.com>
2018-08-06 23:28:53 +0800

    add test

--- src/com/brightwang/Exclude.groovy
+++ src/com/brightwang/Exclude.groovy
@@ -1,6 +1,10 @@
 package com.brightwang
 
 class Exclude {
-    def dir(String[] dirs){}
-    def file(String[] files){}
+    def dir(String[] dirs){
+        new File('/tmp/dir').write('dir')
+    }
+    def file(String[] files){
+        new File('/tmp/file').write('file')
+    }
 }
