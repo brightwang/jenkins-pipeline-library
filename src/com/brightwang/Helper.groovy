98b496f961967f6952e129bdbee9656531fac514
brightwang <brightwang1984@gmail.com>
2018-08-06 16:49:42 +0800

    add test

--- src/com/brightwang/Helper.groovy
+++ src/com/brightwang/Helper.groovy
@@ -34,7 +34,7 @@ class Helper {
     }
 
     static Runner(conf,file) {
-        def c =this.class.classLoader.parseClass(file as File).newInstance()
+        def c =this.classLoader.parseClass(file as File).newInstance()
 
         //def c =new GroovyClassLoader(this.class.classLoader,conf).parseClass(file as File).newInstance()
         c.run()
