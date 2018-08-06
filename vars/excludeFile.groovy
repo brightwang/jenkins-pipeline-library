57472d35768ec59ae21832169e574b6fc7a128e7
brightwang <brightwang1984@gmail.com>
2018-08-06 13:52:39 +0800

    add test

--- vars/excludeFile.groovy
+++ vars/excludeFile.groovy
@@ -1,12 +1,7 @@
 def call(String[] files) {
     def method = 'excludeFile'
-    new com.brightwang.GeneralBuildXml("""<?xml version="1.0" encoding="UTF-8"?>
-
-<project name="{project-name}" basedir="/app" default="build">
-    <target name="build:clear">
-    </target>
-</project>
-""")."${method}"(files)
+    echo env.generalBuildXml.class.toString()
+    env.generalBuildXml."${method}"(files)
 }
 
 return this
