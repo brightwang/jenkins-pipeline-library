a550c2692d8ae1919fc0d4a6443b209e81f1b8f8
brightwang <brightwang1984@gmail.com>
2018-08-07 11:22:27 +0800

    add test

--- vars/dslDemo.groovy
+++ vars/dslDemo.groovy
@@ -73,7 +73,7 @@ def call() {
 //${dsl}
 //""")
     def loader = new GroovyClassLoader(this.class.classLoader, conf)
-    loader.parseClass(dsl).newInstance().run()
+    this.class.classLoader.parseClass(dsl).newInstance().run()
 
 //    env.test=[]
 //    env.xml=xml
